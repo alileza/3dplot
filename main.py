0bc7f55b657194c2183df3f970ba27994a089f97
alileza <alirezayahya@gmail.com>
2020-11-29 22:22:26 +0100

    change color to blue

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import csv
 
-x =[[],[]]
-y =[[],[]]
-z =[[],[]]
+x =[[]]
+y =[[]]
+z =[[]]
 
 with open('/app/data.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -28,10 +28,10 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 for index, value in enumerate(x):
-    ax.scatter(x[index], y[index], z[index], c='r', marker='o')
+    ax.scatter(x[index], y[index], z[index], c='blue', marker='o')
 
-    ax.set_xlabel('X Label')
-    ax.set_ylabel('Y Label')
-    ax.set_zlabel('Z Label')
-
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    
     plt.savefig(f'/out/{index:03d}.png')
